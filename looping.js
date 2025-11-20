92c78d5554879a06ada0046a61390fb64c700d4d
Stephencolao0303 <Stephencolao0303@gmail.com>
2025-11-20 08:29:38 +0800

    added while-loop

--- looping.js
+++ looping.js
@@ -1,19 +1,24 @@
 // for loop 
-output = " ";
+//
+//output = " ";
 
-for(i = i;i<=5 ; i++) {
+//for(i = i;i<=5 ; i++) {
     //console.log(i);
-    output += i + " ";
-}
+   // output += i + " ";
 
-//consoloe.log(output);
+
+//console.log(output);
 
 //Arrays
+output =" ";
 arr = [1,2,3]
+console.log("Array length:" + arr.length);
 
-for(index =0; index <arr.length; index++){
+for(index = arr.length -1; index>=0; index--){
+    // console.log(arr[index]);
+    output += arr[index] + " ";
     //console.log(arr[i]);
-    output += arr[index] + "";
+    
 }
 
 console.log(output);
---
+++ while-loop.js
@@ -0,0 +1,40 @@
+/*
+console.log("Reverse");
+i = 1;
+
+while (i >= 5){
+    console.log(i);
+    i--;
+}
+
+/*
+console.log("Ascending");
+x = 1;
+
+while (x <= 5){
+    console.log(x);
+    x++;
+}
+
+
+/*
+output = " ";
+x = 1;
+while(x<=5){
+    output += x + " ";
+    x++
+}
+console.log(output);
+
+*/
+
+arr=[1,2,3];
+output = " ";
+index = 1;
+
+while(index <= arr.length){
+    output += index + " ";
+    index++;
+}
+
+console.log(output);
