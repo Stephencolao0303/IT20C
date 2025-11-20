d0bb9fadd63bd3b8776963f29c2176ab10cb7eff
Stephencolao0303 <Stephencolao0303@gmail.com>
2025-11-20 08:07:31 +0800

    initial commit

---
+++ cs.js
@@ -0,0 +1,46 @@
+function legalAge(age){
+    if(age >=18){
+console.log("You are an adult")
+}else{
+    console.log("You are not a adult")
+
+}
+}
+
+legalAge(50);
+
+
+
+function computeGrade(grade){
+
+    if(grade >=94.5){
+console.log("1")
+}else if(grade >=91.5){
+    console.log("1.25")
+}else if(grade >=89.5){
+    console.log("1.5")
+    }else if(grade >=86.5){
+    console.log("1.75")
+    }else if(grade >=84.5){
+    console.log("2")
+}else{
+    console.log("Not within the transmutation")
+}
+}
+
+computeGrade(85);
+
+
+
+function login(username, password){
+    if(username === "gel"){
+        console.log("user recognized");
+        if(password ===1234){
+            console.log("Welcome user");
+}else{
+    console.log("Invalid user");
+}
+    }
+}
+
+login("gel",123);
---
+++ looping.js
@@ -0,0 +1,19 @@
+// for loop 
+output = " ";
+
+for(i = i;i<=5 ; i++) {
+    //console.log(i);
+    output += i + " ";
+}
+
+//consoloe.log(output);
+
+//Arrays
+arr = [1,2,3]
+
+for(index =0; index <arr.length; index++){
+    //console.log(arr[i]);
+    output += arr[index] + "";
+}
+
+console.log(output);
